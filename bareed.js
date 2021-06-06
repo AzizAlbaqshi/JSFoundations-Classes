cf32d4b284b4eb1c23b899189c0472002610d290
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-06-06 14:15:10 +0300

    Task , Done

--- bareed.js
+++ bareed.js
@@ -43,11 +43,19 @@ class Point {
  **********************************************************/
 class Wallet {
   // implement Wallet!
-  constructor(money = 0) {}
+  constructor(money = 0) {
+    
+    this.money = money;
+  }
 
-  credit = amount => {};
+  credit = (amount) => {
+   this.money += amount;
+  };
 
-  debit = amount => {};
+  debit = amount => {
+   this.money =  this.money - amount;
+
+  };
 }
 
 /**********************************************************
@@ -62,8 +70,17 @@ class Wallet {
  * let person = new Person(name, x, y);
  **********************************************************/
 class Person {
-  // implement Person!
-}
+  constructor(name,x,y){
+    this.name = name;
+    this.location=new Point(x,y);
+    this.wallet= new Wallet();
+  }
+
+  moveTo = (point) => (this.location = point);
+    
+  }
+
+
 
 /**********************************************************
  * Vendor: defines a vendor
@@ -80,8 +97,19 @@ class Person {
  *
  * new vendor = new Vendor(name, x, y);
  **********************************************************/
-class Vendor {
-  // implement Vendor!
+class Vendor extends Person{
+  constructor(name,x,y){
+    super(name,x,y);
+    this.range=5;
+    this.price=1;
+  }
+  sellTo = (customer, numberOfIceCreams) => {
+    const totalPrice = this.price * numberOfIceCreams;
+    this.moveTo(customer.location);
+
+customer.wallet.debit(totalPrice);
+this.wallet.credit(totalPrice);
+  };
 }
 
 /**********************************************************
@@ -100,10 +128,30 @@ class Vendor {
  *
  * new customer = new Customer(name, x, y);
  **********************************************************/
-class Customer {
-  // implement Customer!
-}
+class Customer extends Person{
+ constructor(name,x,y){
+   
+     super(name,x,y);
+     this.wallet = new Wallet(10);
+   }
+   _isInRange = (Vendor) => {
+     const distance = this.location.distanceTo(Vendor.location);
+     const range = Vendor.range;
+     return distance <= range;
+   };
+ _haveEnoughMoney =(Vendor,numberOfIceCreams) => {
+   const totalPrice = Vendor.price * numberOfIceCreams;
+   return this.wallet.money >= totalPrice;
+ };
 
+ requestIceCream = (Vendor , numberOfIceCreams) => {
+   if (this._isInRange(Vendor)&& 
+       this._haveEnoughMoney(Vendor,numberOfIceCreams)
+       ){
+     Vendor.sellTo(this,numberOfIceCreams);
+ }
+};
+}
 export { Point, Wallet, Person, Customer, Vendor };
 
 /***********************************************************
